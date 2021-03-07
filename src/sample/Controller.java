12fddb8f3720c0a34f8f987df35886f429a9efb9
B <hennyhexservers@gmail.com>
2021-03-07 15:59:27 +0000

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
+public class Controller {
+
+    @FXML
+    private Circle myCircle;
+    private double x;
+    private double y;
+
+    public void up(ActionEvent e){
+        //System.out.println("UP");
+        myCircle.setCenterY(y-=5);
+    }
+
+    public void down(ActionEvent e){
+        //System.out.println("DOWN");
+        myCircle.setCenterY(y+=5);
+    }
+
+    public void left(ActionEvent e){
+        //System.out.println("LEFT");
+        myCircle.setCenterX(x-=5);
+    }
+
+    public void right(ActionEvent e){
+        //System.out.println("RIGHT");
+        myCircle.setCenterX(x+=5);
+    }
+}
